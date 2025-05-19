c5a2c2b6d2afb338ada0bf9ef70705b89a2eede9
svenjajunghans <svenja.junghans@fh-potsdam.de>
2025-05-19 11:57:38 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,52 +1,58 @@
-let audio;
-let fft;
-let amp;
-
-function preload() {
-  // Stelle sicher: Datei heißt genau "relaxing.mp3"
-  audio = loadSound('relaxing.mp3');
-}
-
 function setup() {
-  // ...existing code...
 function setup() {
   createCanvas(800, 600);
   noStroke();
 
   amp = new p5.Amplitude();
-  amp.setInput(audio); // <--- Diese Zeile ergänzen
+  amp.setInput(audio);
   fft = new p5.FFT();
   fft.setInput(audio);
 } 
 }
-// ...existing code...
+
+let blobs = [];
+
+function setup() {
+  createCanvas(800, 600);
+  noFill();
+  for (let i = 0; i < 6; i++) {
+    blobs.push(new Blob(random(width), random(height), random(60, 150)));
+  }
+}
 
 function draw() {
-  background(255);
+  background(250, 248, 245); // warm white tone
 
-  if (audio.isPlaying()) {
-    let level = amp.getLevel();
-    let size = map(level, 0, 1, 100, 400);
+  for (let blob of blobs) {
+    blob.update();
+    blob.display();
+  }
+}
 
-    fill(180, 120, 200, 150);
-    ellipse(width / 2, height / 2, size);
+class Blob {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.offset = random(1000);
+  }
 
-    let waveform = fft.waveform();
-    noFill();
-    stroke(200, 150, 220, 80);
+  update() {
+    this.offset += 0.01;
+  }
+
+  display() {
+    stroke(150, 120, 200, 80);
+    strokeWeight(2);
     beginShape();
-    for (let i = 0; i < waveform.length; i++) {
-      let x = map(i, 0, waveform.length, 0, width);
-      let y = map(waveform[i], -1, 1, height * 0.25, height * 0.75);
+    for (let a = 0; a < TWO_PI; a += 0.1) {
+      let off = this.offset + cos(a) * 0.5;
+      let r = this.r + noise(off) * 40;
+      let x = this.x + cos(a) * r;
+      let y = this.y + sin(a) * r;
       curveVertex(x, y);
     }
-    endShape();
+    endShape(CLOSE);
   }
 }
 
-// ← wichtig: Sound wird erst nach Klick gestartet!
-function mousePressed() {
-  if (audio && !audio.isPlaying()) {
-    audio.loop();
-  }
-}
