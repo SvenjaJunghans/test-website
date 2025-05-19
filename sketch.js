00f8a853efc9cc84d4fb6495bce7b3c12cdd685d
svenjajunghans <svenja.junghans@fh-potsdam.de>
2025-05-19 12:16:41 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,52 +1,47 @@
-function setup() {
-function setup() {
-  createCanvas(800, 600);
-  noStroke();
+let lines = [];
+let numLines = 30;
+let resolution = 300;
 
-  amp = new p5.Amplitude();
-  amp.setInput(audio);
-  fft = new p5.FFT();
-  fft.setInput(audio);
-} 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  stroke(255, 60);
+  strokeWeight(1.2);
+  initLines();
 }
 
-let lines = [];
-
-function setup() {
-  createCanvas(800, 600);
-  stroke(255, 80);
-  noFill();
-
-  for (let i = 0; i < 60; i++) {
-    lines.push(new FlowLine(i * 4));
+function initLines() {
+  lines = [];
+  for (let j = 0; j < numLines; j++) {
+    let line = [];
+    for (let i = 0; i < resolution; i++) {
+      let x = map(i, 0, resolution - 1, 0, width * 2); // doppelte Breite für Loop
+      let y = noise(i * 0.05, j * 0.1) * height;
+      line.push({ x, y });
+    }
+    lines.push(line);
   }
 }
 
 function draw() {
-  background(40); // dunkelgrauer Hintergrund
+  background(30);
+  let scroll = frameCount * 0.5 % width;
 
-  for (let l of lines) {
-    l.update();
-    l.display();
-  }
-}
-
-class FlowLine {
-  constructor(yOffset) {
-    this.yOffset = yOffset;
-    this.t = random(1000);
-  }
-
-  update() {
-    this.t += 0.005;
-  }
-
-  display() {
+  push();
+  translate(-scroll, 0); // endloser horizontaler Loop
+  for (let j = 0; j < lines.length; j++) {
     beginShape();
-    for (let x = 0; x <= width; x += 10) {
-      let y = this.yOffset + noise(x * 0.005, this.t) * 100;
-      curveVertex(x, y);
+    for (let i = 0; i < lines[j].length; i++) {
+      let p = lines[j][i];
+      let offsetY = sin(frameCount * 0.01 + i * 0.05 + j * 0.1) * 40;
+      curveVertex(p.x, p.y + offsetY);
     }
     endShape();
   }
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  initLines(); // Linien neu berechnen bei Resize
 }
